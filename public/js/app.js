d3981e84182960ad984d5e6544e98b99ed83374a
Michal Kovář <michalkvr.dev@gmail.comgit config --global user.name Michal>
2022-03-15 22:31:01 +0100

    [ADD] Select boxes fetched from airtable

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,66 @@
-class PartList {
-  constructor() {
-    this.anchor = document.getElementById('parts');
-  }
-  
-  render(parts) {
-    parts.forEach(part => {
-      const props = part.fields;
-      let el = document.createElement('div');
-      el.innerHTML = `
-        <p>${props.name}</p>
-        <img src="${props.img[0].url}" />
-      `;
-      this.anchor.appendChild(el);
-    })
-  }
+const token = 'keyvt6vx1nJeHCRBX';
+const api = 'https://api.airtable.com/v0/appnxwDTMUtlFZXdI';
+
+fetchData('/machinery_make', renderMachineryMake);
+fetchData('/type_of_machinery', renderTypeOfMachinery);
+fetchData('/type_of_part', renderTypeOfPart);
+fetchData('/parts', renderParts);
+
+function renderMachineryMake(items, ) {
+  const anchor = document.getElementById('machineryMake');
+  renderMenu(items, anchor);
 }
 
-parts = new PartList();
+function renderTypeOfMachinery(items, ) {
+  const anchor = document.getElementById('typeOfMachinery');
+  renderMenu(items, anchor);
+}
 
-const token = 'keyvt6vx1nJeHCRBX';
-const myHeaders = new Headers();
-myHeaders.append('Accept', 'application/json');
-myHeaders.append('Authorization', 'Bearer ' + token);
+function renderTypeOfPart(items, ) {
+  const anchor = document.getElementById('typeOfParts');
+  renderMenu(items, anchor);
+}
 
-const myInit = {
-  method: 'GET',
-  headers: myHeaders,
+function renderMenu(items, anchor) {
+  items.forEach((item) => {
+    const props = item.fields;
+    let el = document.createElement('option');
+    el.setAttribute('value', props.code)
+    el.innerHTML = props.name;
+    anchor.appendChild(el);
+  });
 };
 
-fetch('https://api.airtable.com/v0/appnxwDTMUtlFZXdI/parts', myInit)
-  .then(function(response) { return response.json(); })
-  .then(function(data) { 
-    parts.render(data.records);
+function renderParts(items, ctx) {
+  const anchor = document.getElementById('parts');
+  items.forEach((item) => {
+    const props = item.fields;
+    let el = document.createElement('div');
+    el.innerHTML = `
+      <p>${props.part_number}</p>
+      <p>${props.name}</p>
+      <img src="${props.img[0].url}" />
+      <p>${props.description}</p>
+      <p>${props.price}</p>
+      <p>${props.in_stock}</p>
+    `;
+    anchor.appendChild(el);
   });
+};
+
+function fetchData(route, callback) {
+  const myHeaders = new Headers();
+  myHeaders.append('Accept', 'application/json');
+  myHeaders.append('Authorization', 'Bearer ' + token);
+  const myInit = {
+    method: 'GET',
+    headers: myHeaders,
+  };
+  fetch(api + route, myInit)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      callback(data.records);
+    });
+};
